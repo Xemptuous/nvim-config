1d10d4c5b86186ae51205c8ed68b8e95d85ec89b
Xemptuous <99044751+Xemptuous@users.noreply.github.com>
2025-02-04 14:02:11 -0800

    feat: added bufdelete.nvim

--- lua/plugins/utils.lua
+++ lua/plugins/utils.lua
@@ -89,6 +89,15 @@ return {
     },
     { "tpope/vim-abolish", keys = "cr" },
     { "kevinhwang91/nvim-bqf", ft = { "qf" } },
+    {
+        "famiu/bufdelete.nvim",
+        event = "VeryLazy",
+        init = function()
+            local map = vim.keymap.set
+            map("n", "<leader>bd", "<cmd>lua require('bufdelete').bufdelete(0, true)<cr>", { desc = "Buf Delete" })
+            map("n", "<leader>bw", "<cmd>lua require('bufdelete').bufwipeout(0)<cr>", { desc = "Buf Wipeout" })
+        end,
+    },
     -- { "jidn/vim-dbml", ft = { "dbml" } },
     -- { "chrisbra/unicode.vim", event = "VeryLazy" },
     {
