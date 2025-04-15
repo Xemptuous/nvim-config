b71ec983810a57a8783270c59ca2706708925dfc
Xemptuous <99044751+Xemptuous@users.noreply.github.com>
2025-04-15 07:28:41 -0700

    basedpyright settings

--- lsp/basedpyright.lua
+++ lsp/basedpyright.lua
@@ -1,4 +1,16 @@
 return {
     cmd = { "basedpyright-langserver", "--stdio" },
+    settings = {
+        basedpyright = {
+            analysis = {
+                autoSearchPaths = true,
+                useLibraryCodeForTypes = true,
+                autoImportCompletions = true,
+                typeCheckingMode = "standard", -- off, basic, standard, strict, recommended, all
+                -- typeCheckingMode = "off", -- off, basic, standard, strict, recommended, all
+                -- diagnosticMode = "openFilesOnly",
+            },
+        },
+    },
     single_file_support = true,
 }
