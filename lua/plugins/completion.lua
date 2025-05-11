9abb46221fd6f985f66e34213de1344b945b9259
Xemptuous <99044751+Xemptuous@users.noreply.github.com>
2025-05-10 22:54:52 -0700

    renamed cmp due to minuet conflict

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -1,107 +1,134 @@
 return {
     {
         "saghen/blink.cmp",
-        enabled = true,
         event = { "InsertEnter", "CmdlineEnter" },
         dependencies = {
             "rafamadriz/friendly-snippets",
+            "milanglacier/minuet-ai.nvim",
             "onsails/lspkind.nvim",
-            {
-                "xzbdmw/colorful-menu.nvim",
-                opts = {},
-            },
+            "Kaiser-Yang/blink-cmp-avante",
+            { "xzbdmw/colorful-menu.nvim", opts = {} },
         },
         version = "1.*",
         -- build = "cargo build --release",
 
         opts_extend = { "sources.default" },
-        opts = {
-            sources = { default = { "lsp", "path", "snippets", "buffer", "cmdline" } },
-            -- sources = { default = { "lsp", "path", "snippets", "buffer", "cmdline", "omni" } },
-            keymap = {
-                preset = "super-tab",
-                ["<Tab>"] = {
-                    function(cmp)
-                        if cmp.snippet_active() or cmp.is_visible() then
-                            return cmp.accept()
-                        else
-                            return cmp.select_and_accept()
-                        end
-                    end,
-                    "snippet_forward",
-                    "fallback",
-                },
-                -- ["<CR>"] = { "accept", "fallback" },
-            },
-            signature = {
-                enabled = true,
-                trigger = {
-                    show_on_insert = true,
-                },
-                window = {
-                    border = "single",
-                    winhighlight = "Normal:Normal,FloatBorder:FloatBorder",
-                },
-            },
-            cmdline = {
-                enabled = true,
+        opts = function()
+            local b = require("blink.cmp").add_source_provider("minuet", { name = "minuet", module = "minuet.blink" })
+            return {
                 keymap = {
-                    preset = "inherit",
-                },
-                completion = {
-                    menu = {
-                        auto_show = true,
+                    preset = "super-tab",
+                    ["<Tab>"] = {
+                        function(cmp)
+                            if cmp.snippet_active() or cmp.is_visible() then
+                                return cmp.accept()
+                            else
+                                return cmp.select_and_accept()
+                            end
+                        end,
+                        "snippet_forward",
+                        "fallback",
                     },
+                    ["<C-y>"] = {
+                        function(cmp) cmp.show({ providers = { "minuet" } }) end,
+                    },
+                    -- ["<CR>"] = { "accept", "fallback" },
                 },
-            },
-            appearance = {
-                use_nvim_cmp_as_default = false,
-                nerd_font_variant = "mono",
-            },
-            completion = {
-                documentation = {
-                    auto_show = true,
-                    auto_show_delay_ms = 250,
-                    update_delay_ms = 50,
+                sources = {
+                    providers = {
+                        avante = {
+                            module = "blink-cmp-avante",
+                            name = "Avante",
+                            opts = {},
+                        },
+                        minuet = {
+                            name = "minuet",
+                            module = "minuet.blink",
+                            async = true,
+                            -- Should match minuet.config.request_timeout * 1000,
+                            -- since minuet.config.request_timeout is in seconds
+                            timeout_ms = 3000,
+                            score_offset = 50, -- Gives minuet higher priority among suggestions
+                        },
+                    },
+                    default = { "avante", "lsp", "path", "snippets", "buffer", "minuet", "cmdline" },
+                    -- default = { "lsp", "path", "snippets", "buffer", "minuet", "cmdline" },
+                },
+                -- sources = { default = { "lsp", "path", "snippets", "buffer", "cmdline" } },
+                signature = {
+                    enabled = true,
+                    trigger = {
+                        show_on_insert = true,
+                    },
                     window = {
                         border = "single",
-                        winblend = 10,
+                        winhighlight = "Normal:Normal,FloatBorder:FloatBorder",
                     },
                 },
-                keyword = { range = "full" },
-                trigger = { show_in_snippet = false },
-                accept = { auto_brackets = { enabled = false } },
-                menu = {
-                    border = "padded",
-                    winblend = 0,
-                    winhighlight = "Normal:Normal,FloatBorder:FloatBorder,CursorLine:BlinkCmpMenuSelection,Search:None",
-                    scrolloff = 2,
-                    auto_show = true,
-                    draw = {
-                        -- We don't need label_description now because label and label_description are already
-                        -- combined together in label by colorful-menu.nvim.
-                        columns = {
-                            { "kind_icon" },
-                            { "label", gap = 1 },
-                            -- { "label", "label_description", "kind", gap = 1 },
-                        },
-                        components = {
-                            kind_icon = {
-                                ellipsis = false,
-                                text = function(ctx)
-                                    -- local kind_icon, _, _ = require("mini.icons").get("lsp", ctx.kind)
-                                    return require("lspkind").symbolic(ctx.kind, { mode = "symbol" })
-                                end,
-                            },
-                            label = {
-                                text = function(ctx) return require("colorful-menu").blink_components_text(ctx) end,
-                                highlight = function(ctx) return require("colorful-menu").blink_components_highlight(ctx) end,
-                            },
+                cmdline = {
+                    enabled = true,
+                    keymap = {
+                        preset = "inherit",
+                    },
+                    completion = {
+                        menu = {
+                            auto_show = true,
                         },
                     },
                 },
-            },
-        },
+                appearance = {
+                    use_nvim_cmp_as_default = false,
+                    nerd_font_variant = "mono",
+                },
+                completion = {
+                    documentation = {
+                        auto_show = true,
+                        auto_show_delay_ms = 250,
+                        update_delay_ms = 50,
+                        window = {
+                            border = "single",
+                            winblend = 10,
+                        },
+                    },
+                    keyword = { range = "full" },
+                    trigger = {
+                        show_in_snippet = false,
+                        prefetch_on_insert = false,
+                    },
+                    accept = { auto_brackets = { enabled = false } },
+                    menu = {
+                        border = "padded",
+                        winblend = 0,
+                        winhighlight = "Normal:Normal,FloatBorder:FloatBorder,CursorLine:BlinkCmpMenuSelection,Search:None",
+                        scrolloff = 2,
+                        auto_show = true,
+                        draw = {
+                            -- We don't need label_description now because label and label_description are already
+                            -- combined together in label by colorful-menu.nvim.
+                            columns = {
+                                { "kind_icon" },
+                                { "label", gap = 1 },
+                                -- { "label", "label_description", "kind", gap = 1 },
+                            },
+                            components = {
+                                kind_icon = {
+                                    ellipsis = false,
+                                    text = function(ctx)
+                                        -- local kind_icon, _, _ = require("mini.icons").get("lsp", ctx.kind)
+                                        return require("lspkind").symbolic(ctx.kind, { mode = "symbol" })
+                                    end,
+                                },
+                                label = {
+                                    text = function(ctx) return require("colorful-menu").blink_components_text(ctx) end,
+                                    highlight = function(ctx) return require("colorful-menu").blink_components_highlight(ctx) end,
+                                },
+                            },
+                        },
+                    },
+                },
+            }
+        end,
+        config = function(_, opts) require("blink.cmp").setup(opts) end,
     },
 
     {
