8c77538bcf64d5617f4fc25dbc61ae26c2eacc12
Xemptuous <99044751+Xemptuous@users.noreply.github.com>
2025-09-21 22:36:35 -0700

    gopls updates

--- lsp/gopls.lua
+++ lsp/gopls.lua
@@ -1,4 +1,11 @@
 return {
     cmd = { "gopls" },
+    filetypes = { "go", "gomod", "gowork" },
+    root_markers = { { "go.mod", "go.sum", ".git" } },
     single_file_support = true,
+    settings = {
+        gopls = {
+            staticcheck = true,
+        },
+    },
 }
